6e0fff9f02dfc20baa9d6b3046a49e7548573847
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-11 17:26:31 -0500

    recursion, steps, pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n,row= 0,level='') {
+    const mid = Math.floor((2 *n -1)/2)
+    if( n=== row){
+        return
+    }
+    if(level.length === 2 * n - 1){
+        console.log(level)
+        return pyramid(n,row+1)
+    }
+    if(mid - row <= level.length && mid+row >= level.length){
+        level += '#'
+    }else{
+        level += ' '
+    }
+
+    pyramid(n,row,level)
+
+}
 
 module.exports = pyramid;
+
+/*
+function pyramid(n) {
+    const mid = Math.floor((2 *n -1)/2)
+    for (let row = 0;row< n ;row++){
+        let level = ''
+        for (let column = 0; column < 2* n -1; column++){
+            if(mid-row <= column && mid+row >=column){
+                level += '#'
+            }else{
+                level += ' '
+            }
+        }
+        console.log(level)
+    }
+}*/
