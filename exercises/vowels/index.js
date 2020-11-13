1551a37d6517cfc815c32bada48dc8b9ef82696f
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-13 16:42:58 -0500

    vowels, matrix

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    //g : don`t stop at the first match, go on
+    //i : case insensitive
+    const matches = str.match(/[aeiou]/gi)
+
+    return matches ? matches.length : 0
+
+}
 
 module.exports = vowels;
+
+/*
+function vowels(str) {
+    const vowels = ['a','e','i','o','u']
+    let count = 0
+    return str.split('').reduce((a, c) => {
+        if (vowels.includes(c.toLowerCase())) {
+            count += 1
+        }
+        a = count
+        return a
+    }, 0)
+}
+ */
