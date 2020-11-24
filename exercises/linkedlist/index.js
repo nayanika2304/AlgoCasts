bd8fdb6f3a95f75468867d4a3f2a698076057a1d
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-24 17:45:49 -0500

    started implementing linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,56 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data,next = null) {
+        this.data = data
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+
+    insertFirst(data){
+        this.head  = new Node(data,this.head)
+    }
+
+    size(){
+        let size = 0
+
+        if(!this.head){
+            return size
+        }
+        while(this.head){
+            this.head = this.head.next
+            size+=1
+        }
+
+        return size
+    }
+
+    getFirst(){
+        return this.head
+    }
+
+    getLast(){
+        let node = this.head
+        if(!node){
+            return null
+        }
+        while(node){
+            if(!node.next){
+                return node
+            }
+            node = node.next
+        }
+    }
+
+    clear(){
+        this.head = null
+    }
+}
 
 module.exports = { Node, LinkedList };
