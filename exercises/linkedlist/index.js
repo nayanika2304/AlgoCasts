3a00752b80304d6ca39274a9fee59a271674453c
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-25 17:01:29 -0500

    started implementing linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -14,43 +14,97 @@ class LinkedList {
         this.head = null
     }
 
-    insertFirst(data){
-        this.head  = new Node(data,this.head)
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
     }
 
-    size(){
-        let size = 0
+    size() {
+        let counter = 0;
+        let node = this.head;
 
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        this.head = this.head.next;
+    }
+
+    removeLast(){
         if(!this.head){
-            return size
-        }
-        while(this.head){
-            this.head = this.head.next
-            size+=1
-        }
-
-        return size
-    }
-
-    getFirst(){
-        return this.head
-    }
-
-    getLast(){
-        let node = this.head
-        if(!node){
             return null
         }
-        while(node){
-            if(!node.next){
-                return node
-            }
+        if(!this.head.next){
+            this.head = null
+            return
+        }
+        let previous = this.head
+        let node = this.head.next
+        while(node.next){
+            previous = node;
             node = node.next
+        }
+        previous.next = null
+    }
+
+    insertLast(data){
+        //let newNode = new Node(data)
+        const last = this.getLast()
+        if(!last){
+            //the chain is empty
+            this.head = new Node(data)
+        }else{
+            // some existing nodes in the chain
+            last.next = new Node(data)
         }
     }
 
-    clear(){
-        this.head = null
+    getAt(index){
+        let node = this.head
+        let counter = 0
+
+        if(!this.head){
+            return null
+        }
+
+        while(node){
+            if(counter === index){
+                return node
+            }
+            counter ++
+            node = node.next
+        }
+        return null
     }
 }
 
