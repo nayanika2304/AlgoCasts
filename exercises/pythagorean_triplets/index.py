ff5e088117c70d687f6e951de263cbc846f98939
nayanika2304 <nayanikabhargava1993@gmail.com>
2021-02-17 15:46:26 -0500

    new exercises

---
+++ exercises/pythagorean_triplets/index.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def badPythagoreanTriplets(self, nums):
+        for a in nums:
+            for b in nums:
+                for c in nums:
+                    if a*a + b*b == c*c:
+                        return True
+        return False
+
+
+    def goodPythagoreanTriplets(self, nums):
+        squares = set([n*n for n in nums])
+
+        for a in nums:
+            for b in nums:
+                if a*a + b*b in squares:
+                    return True
+        return False
+
+
+
+print(Solution().badPythagoreanTriplets([3, 5, 12, 5, 13]))
+print(Solution().goodPythagoreanTriplets([3, 5, 12, 5, 13]))
