3cc38ae051f7d23dab6ab4a8ff78115900430b00
nayanika2304 <nayanikabhargava1993@gmail.com>
2021-05-12 12:44:45 -0400

    new exercises

---
+++ exercises/nearest_smallest_element/index.py
@@ -0,0 +1,33 @@
+import math as mt
+
+# Prints smaller elements on left
+# side of every element
+def printPrevSmaller(arr, n):
+
+    # Create an empty stack
+    S = list()
+
+    # Traverse all array elements
+    for i in range(n):
+
+        # Keep removing top element from S
+        # while the top element is greater
+        # than or equal to arr[i]
+        while (len(S) > 0 and S[-1] >= arr[i]):
+            S.pop()
+
+        # If all elements in S were greater
+        # than arr[i]
+        if (len(S) == 0):
+            print("_, ", end = "")
+        else: # Else print the nearest
+            # smaller element
+            print(S[-1], end = ", ")
+
+        # Push this element
+        S.append(arr[i])
+
+# Driver Code
+arr = [ 1, 3, 0, 2, 5]
+n = len(arr)
+printPrevSmaller(arr, n)
